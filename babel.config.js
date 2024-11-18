db5cf4f10919ecfa2bd11e692c47fba92391d13e
Shin <skynet3285@gmail.com>
2024-11-18 09:26:40 +0900

    feat: env

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,21 @@
 module.exports = {
   presets: ["module:@react-native/babel-preset"],
-  plugins: ["nativewind/babel"],
+  plugins: [
+    "nativewind/babel",
+    [
+      "module:react-native-dotenv",
+      {
+        envName: "APP_ENV",
+        moduleName: "@env",
+        path: ".env.local",
+        blocklist: null,
+        allowlist: null,
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+  ],
 };
---
+++ src/env.ts
@@ -0,0 +1,7 @@
+import { API_URL } from "@env";
+
+const env = {
+  API_URL,
+};
+
+export default env;
